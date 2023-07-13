1afdb142528c656a6c5fc550f51a0207b0afedae
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-13 13:47:14 +0200

    Improve apptainer run script and add friends

--- run-apptainer.sh
+++ run-apptainer.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 # get the Comp directory's full name.
 # needed as Comp* is not always expanded when 
-# needed beloww
-comp_dir=$(ls .. | grep -e "^Comp")
-test -e ../"$comp_dir"/sif/r423.sif || ../"$comp_dir"/scripts/ubuntu/download_apptainer.sh
-# set R_LIBS if not set as otherwise
-# it is by default set to a non-writeable 
-# directory in the image
-R_LIBS=${R_LIBS:=$HOME/.local/.cache/R}
-# need to create the directory, as otherwise R_LIBS
-# will have no effect
-mkdir -p $R_LIBS
+# needed below
+date
+
+# Function to check if sif is in there and then return the path if it is
+get_path_sif() {
+  # Check if directory exists
+  if [ -d "$1" ]; then
+    # If it does exist, then check if it has a sif file
+    if [ -n "$(ls "$1" | grep -E 'sif$')" ]; then
+      echo "$1/$(ls "$1" | grep -E 'sif$')"
+    else
+      echo "File not found" >&2
+      exit 1
+    fi
+  else
+    echo "Directory does not exist" >&2
+    exit 1
+  fi
+}
+
+# Attempt to get path to sif in ./sif
+echo "Attempting to get path to sif in ./sif"
+path_sif=$(get_path_sif "sif")
+
+# If path_sif is empty, attempt to get path to sif in ../$comp_dir/sif
+if [ -z "$path_sif" ]; then
+  comp_dir=$(ls .. | grep -e "^Comp")
+  echo "Attempting to get path to sif in ../$comp_dir/sif"
+  path_sif=$(get_path_sif "../$comp_dir/sif")
+fi
+
+# Exit if sif file not found
+test -n "$path_sif" || { echo "sif file not found"; exit 1; }
+
+echo "Final path to sif: $path_sif"
+
 if [ -n "$(env | grep -E "^GITPOD|^CODESPACE")" ]; then
   export RENV_CONFIG_PAK_ENABLED=${RENV_CONFIG_PAK_ENABLED:=TRUE}
 fi
-export GITHUB_PAT=${GITHUB_PAT:=$GH_TOKEN} 
-if which apptainer > /dev/null 2>&1; then
-  apptainer run ../"$comp_dir"/sif/r423.sif ${1:-code-insiders tunnel --accept-server-license-terms}
-elif which singularity > /dev/null 2>&1; then
-  singularity run ../"$comp_dir"/sif/r423.sif ${1:-code-insiders tunnel --accept-server-license-terms}
+
+# Run script if sif file found
+path_script="scripts/R/faust-lymph-v1.R"
+# Run apptainer if it is found, and singularity if apptainer is not.
+# Exit with an error if neither is found
+if command -v apptainer > /dev/null 2>&1; then
+  echo "Running apptainer"
+  apptainer run "$path_sif" "$1"
+elif command -v singularity > /dev/null 2>&1; then
+  echo "Running singularity"
+  singularity run "$path_sif" "$1"
 else
   echo "Neither singularity nor apptainer container runtime detected"
   exit 1
