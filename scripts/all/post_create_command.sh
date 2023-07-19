258471ef77ad3f4f672ed2641c99469b6b8cedf5
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-19 14:54:53 +0200

    Set up GitPod with R by default

--- scripts/all/post_create_command.sh
+++ scripts/all/post_create_command.sh
@@ -6,10 +6,23 @@
 if ! [ -e "$HOME/.bashrc.d/config_r.sh" ]; then
   cp ./scripts/all/config_r.sh "$HOME/.bashrc.d/"
 fi
-if [ -n "$(env | grep -E "^GITPOD")" ]; then
-  # install tools to run and download containers
-  ./scripts/ubuntu/install_apptainer.sh
-  ./scripts/ubuntu/install_gh.sh
-fi
+#if [ -n "$(env | grep -E "^GITPOD")" ]; then
+# install tools to run and download containers
+./scripts/ubuntu/install_apptainer.sh
+./scripts/ubuntu/install_gh.sh
+#fi
+
+sudo ./scripts/all/install_r.sh \
+  --VERSION 4.3.0 \
+  --VSCODERSUPPORT full \
+  --INSTALLDEVTOOLS true \
+  --INSTALLRENV true \
+  --INSTALLRMARKDOWN true \
+  --INSTALLRADIAN true \
+  --INSTALLVSCDEBUGGER true
+
+sudo ./scripts/all/install_quarto.sh \
+  --INSTALLTINYTEX true
+
 # clone all repos
 ./clone-repos.sh
