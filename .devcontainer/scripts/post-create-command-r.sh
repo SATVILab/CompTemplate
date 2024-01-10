ffa7e19a9778c0d1e20bea0b4b85714d93129a83
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-01-10 12:20:54 +0200

    Install background packages without `pak`

--- .devcontainer/scripts/post-create-command-r.sh
+++ .devcontainer/scripts/post-create-command-r.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Last modified: 2023 Dec 13
+# Last modified: 2024 January 10
 
 # 1. ensure that key VS Code packages are up to date.
 # and does not take long to install.
@@ -10,6 +10,7 @@
 project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)" 
 
 pushd "$HOME"
-Rscript -e 'install.packages(c("jsonlite", "languageserver", "pak"))' \
+Rscript -e 'Sys.setenv("RENV_CONFIG_PAK_ENABLED" = "false")' \
+  -e 'install.packages(c("jsonlite", "languageserver", "pak"))' \
   -e 'remotes::install_github("rstudio/renv")'
 popd
