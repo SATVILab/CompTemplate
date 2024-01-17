7781a6f034c677e92eb953bdfc74d2877d7f7e7c
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-01-17 13:00:02 +0000

    Add repos from XetHub as well to workspace file

--- .devcontainer/scripts/add-repos.sh
+++ .devcontainer/scripts/add-repos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Last modified: 2023 Nov 24
+# Last modified: 2024 Jan 17
 
 # This script is used to add repositories to a workspace JSON file,
 # which can be used to open all repos in a multi-root VS Code workspace.
@@ -18,24 +18,35 @@ if [ ! -f "$workspace_file" ]; then
   echo '{"folders": [{"path": "."}]}' > "$workspace_file"
 fi
 
-# Read and process each line from repos-to-clone.list
-while IFS= read -r repo || [ -n "$repo" ]; do
+add_to_workspace() {
 
-  # Skip lines that are empty, contain only whitespace, or start with a hash
-  if [[ -z "$repo" || "$repo" =~ ^[[:space:]]*# || "$repo" =~ ^[[:space:]]+$ ]]; then
-    continue
-  fi
+  # Read and process each line from the input file
+  while IFS= read -r repo || [ -n "$repo" ]; do
 
-  # Extract the repository name and create the path
-  repo_name="${repo##*/}"
-  repo_path="../$repo_name"
+    # Skip lines that are empty, contain only whitespace, or start with a hash
+    if [[ -z "$repo" || "$repo" =~ ^[[:space:]]*# || "$repo" =~ ^[[:space:]]+$ ]]; then
+      continue
+    fi
 
-  # Check if the path is already in the workspace file
-  if jq -e --arg path "$repo_path" '.folders[] | select(.path == $path) | length > 0' "$workspace_file" > /dev/null; then
-    continue
-  fi
+    # Extract the repository name and create the path
+    repo_name="${repo##*/}"
+    repo_path="../$repo_name"
 
-  # Add the path to the workspace JSON file
-  jq --arg path "$repo_path" '.folders += [{"path": $path}]' "$workspace_file" > temp.json && mv temp.json "$workspace_file"
-done < "./repos-to-clone.list"
+    # Check if the path is already in the workspace file
+    if jq -e --arg path "$repo_path" '.folders[] | select(.path == $path) | length > 0' "$workspace_file" > /dev/null; then
+      continue
+    fi
 
+    # Add the path to the workspace JSON file
+    jq --arg path "$repo_path" '.folders += [{"path": $path}]' "$workspace_file" > temp.json && mv temp.json "$workspace_file"
+  done < "$1"
+}
+
+# Attempt to add from these files if they exist
+if [ -f "./repos-to-clone.list" ]; then
+  add_to_workspace "./repos-to-clone.list"
+fi
+
+if [ -f "./repos-to-clone-xethub.list" ]; then
+  add_to_workspace "./repos-to-clone-xethub.list"
+fi
